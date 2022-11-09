833531c40b4a812d107633ba04d7da3928473a0f
Philipp Fritsche <ph.fritsche@gmail.com>
2022-11-09 09:22:05 +0100

    wip: provide setup helpers

---
+++ src/index.ts
@@ -0,0 +1,88 @@
+import path from 'path'
+import { Builder, BuildProvider, connectDependencyBuilder, createDependencyBuilder, createNodePolyfillBuilder, createSourceBuilder } from './builder'
+import { FileProvider, FileServer, FsFileProvider, HttpFileServer } from './server'
+
+export async function serveDir(dir: string) {
+    const provider = new FsFileProvider(dir)
+    const server = new HttpFileServer(provider)
+
+    return {
+        provider,
+        server,
+        url: await server.url,
+    }
+}
+
+export async function serveToolboxRunner() {
+    const dir = path.dirname((new URL(import.meta.url)).pathname)
+    const s = await serveDir(dir)
+    return {
+        ...s,
+        url: `${s.url}/test/runner/index.js`
+    }
+}
+
+export function createProjectBuildProvider(
+    watchedFiles: string[],
+    {
+        tsConfigFile,
+        sourceBuilderFactory = () => createSourceBuilder({tsConfigFile}),
+        dependencyBuilderFactory = (source) => {
+            const b = createDependencyBuilder()
+            connectDependencyBuilder(source, b)
+            return b
+        },
+        nodeCoreBuilderFactory = (dependencies) => {
+            const b = createNodePolyfillBuilder()
+            connectDependencyBuilder(dependencies, b)
+            return b
+        },
+        fileProviderFactory = () => new FileProvider(),
+        fileServerFactory = (provider) => new HttpFileServer(provider),
+    }: {
+        tsConfigFile: string,
+        sourceBuilderFactory?: () => Builder,
+        dependencyBuilderFactory?: (source: Builder) => Builder,
+        nodeCoreBuilderFactory?: (dependencies: Builder, source: Builder) => Builder,
+        fileProviderFactory?: () => FileProvider,
+        fileServerFactory?: (provider: FileProvider) => FileServer,
+    },
+) {
+    const sourceBuilder = sourceBuilderFactory()
+    const dependencyBuilder = dependencyBuilderFactory(sourceBuilder)
+    const nodeCoreBuilder = nodeCoreBuilderFactory(dependencyBuilder, sourceBuilder)
+
+    const buildProvider = new BuildProvider(watchedFiles)
+    buildProvider.connect(sourceBuilder, watchedFiles)
+    buildProvider.connect(dependencyBuilder, [])
+    buildProvider.connect(nodeCoreBuilder, [])
+
+    const fileProvider = fileProviderFactory()
+    const fileServer = fileServerFactory(fileProvider)
+
+    buildProvider.emitter.addListener('done', ({pending}) => {
+        if (pending) {
+            return
+        }
+
+        for (const b of [sourceBuilder, dependencyBuilder, nodeCoreBuilder]) {
+            for (const [f, v] of b.outputFiles.entries()) {
+                fileProvider.files.set(f, Promise.resolve(v))
+            }
+        }
+    })
+
+    return {
+        buildProvider,
+        fileProvider,
+        fileServer,
+        onBuildDone: (callback: () => Promise<void>) => {
+            buildProvider.emitter.addListener('done', ({pending}) => {
+                if (pending) {
+                    return
+                }
+                callback()
+            })
+        },
+    }
+}
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,5 @@
+export { FileProvider } from './FileProvider'
+export { FsFileProvider } from './FsFileProvider'
 export { FileServer } from './FileServer'
 export { HttpFileServer } from './HttpFileServer'
 export { TmpFileServer } from './TmpFileServer'
