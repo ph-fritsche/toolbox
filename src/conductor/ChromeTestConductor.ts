65a5cf0612256d7f1ae395f4370bf1ed880925da
Philipp Fritsche <ph.fritsche@gmail.com>
2023-11-10 11:12:19 +0000

    fix(deps): update `puppeteer-core`

--- src/conductor/ChromeTestConductor.ts
+++ src/conductor/ChromeTestConductor.ts
@@ -25,7 +25,6 @@ export class ChromeTestConductor extends TestConductor {
         args: [
             '--no-sandbox',
             '--disable-gpu',
-            '--headless',
         ],
     })
 
